9e76c2e03802934ed054daca8fe973721bbebd29
Lorenzo <45638078+PendragonLore@users.noreply.github.com>
2019-07-05 17:47:27 +0200

    Test push

--- setup.py
+++ setup.py
@@ -24,12 +24,12 @@ if version.endswith(("a", "b", "rc")):
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
-            version += out.decode('utf-8').strip()
+            version += out.decode("utf-8").strip()
         p = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         if out:
-            version += '+g' + out.decode('utf-8').strip()
+            version += "+g" + out.decode("utf-8").strip()
     except Exception:
         pass
 
