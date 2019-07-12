746eb1d3436792491c816a5d62c971ade05a1504
Lorenzo <45638078+PendragonLore@users.noreply.github.com>
2019-07-12 22:04:18 +0200

    Hopefully finish up CI.

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [[ -n "${ON_ALPINE}" ]]; then
+if [ -n "${ON_ALPINE}" ]; then
     apk add --no-cache --virtual .build-deps gcc musl-dev linux-headers make
 fi
 pip install .[ujson,docs,tests] -U wheel
---
+++ .ci/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+PYTHONPATH="$(pwd)" pytest -vs
+isort . --check-only --recursive
+flake8 .
+mypy -p shinkei
+python ./setup.py sdist bdist_egg bdist_wheel
+cd docs && make html
